5252bbffc9a4759a73a3e9df5189700788b92676
Jan Starke <jan.starke@t-systems.com>
2022-12-23 01:26:13 +0100

    better highlighting

--- src/evtxls.rs
+++ src/evtxls.rs
@@ -1,16 +1,17 @@
 use std::{
     io::{Read, Seek},
-    path::PathBuf, net::Ipv4Addr, str::FromStr,
+    path::PathBuf, net::Ipv4Addr, str::FromStr, collections::HashMap,
 };
 
 use anyhow::Result;
 use clap::Parser;
-use colored::{Colorize, control::SHOULD_COLORIZE};
+use colored::{Colorize, control::SHOULD_COLORIZE, ColoredString};
 use data::EventId;
 use evtx::{EvtxParser, SerializedEvtxRecord};
 mod data;
 
-use serde_json::Value;
+use serde::Serialize;
+use serde_json::{Value, json};
 use lazy_regex::regex;
 
 /// Display one or more events from an evtx file
@@ -74,45 +75,91 @@ fn display_results<T: Read + Seek>(mut parser: EvtxParser<T>, cli: &Cli) -> Resu
     Ok(())
 }
 
-fn display_record(record: &SerializedEvtxRecord<Value>, cli: &Cli) -> Result<()> {
+struct HighlightedString {
+    cstring: ColoredString
+}
+
+impl From<ColoredString> for HighlightedString {
+    fn from(cstring: ColoredString) -> Self {
+        Self {
+            cstring
+        }
+    }
+}
+
+impl Serialize for HighlightedString {
+    fn serialize<S>(&self, serializer: S) -> Result<S::Ok, S::Error>
+    where
+        S: serde::Serializer {
+        let s = self.cstring.to_string();
+        serializer.serialize_str(&s)
+    }
+}
+
+fn highlight_string(s: &str) -> HighlightedString {
     let ip_regex = regex!(r"\b(?:(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.){3}(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\b");
+    if ip_regex.is_match(s) {
+        for c in ip_regex.captures_iter(s) {
+            for m in c.iter().flatten() {
+                let ip_addr = Ipv4Addr::from_str(m.as_str()).unwrap();
+                if ip_addr.is_link_local() || ip_addr.is_loopback() {
+                    continue;
+                }
+                if ip_addr.is_private() {
+                    return s.bright_purple().into();
+                }
+                //if ip_addr.is_global() {
+                    return s.bright_red().on_yellow().into();
+                //}
+            }
+        }
+    }
+
+    if s.to_lowercase().contains("admin") {
+        return s.bright_yellow().on_red().into()
+    }
+
+    s.bright_blue().into()
+}
+
+fn highlight_data(data: &Value) -> Value {
+    match data {
+        Value::String(s) => json!(highlight_string(s)),
+        Value::Array(o) => {
+            let s: Vec<_> = o.iter().map(highlight_data).collect();
+            json!(s)
+        }
+        Value::Object(o) => {
+            let s: HashMap<_, _> = o.iter()
+                .map(|(k,v)| (
+                    k, json!(highlight_data(v))
+                )).collect();
+            json!(s)
+        },
+        _ => data.clone(),
+    }
+}
+
+fn display_record(record: &SerializedEvtxRecord<Value>, cli: &Cli) -> Result<()> {
     let size = record.data.to_string().len();
     let event_id = EventId::try_from(record)?;
     let user_data = record.data["Event"]
         .get("UserData")
-        .map(|user_data| user_data.to_string());
+        .map(|user_data| highlight_data(user_data).to_string());
 
     let event_data = record.data["Event"]
         .get("EventData")
-        .map(|event_data| event_data.to_string())
+        .map(|event_data| highlight_data(event_data).to_string())
         .or(user_data.or(Some("".to_owned())))
         .unwrap().normal();
+    
+    let event_data = event_data.replace("\\u001b", "\u{001b}");
     
     let event_id = if event_id == 4624.into() {
         event_id.to_string().bright_yellow()
     } else {
         event_id.to_string().normal()
     };
-
-    let mut colored_event_data = None;
-    if ip_regex.is_match(&event_data) {
-        'outer: for c in ip_regex.captures_iter(&event_data) {
-            for m in c.iter().flatten() {
-                let ip_addr = Ipv4Addr::from_str(m.as_str())?;
-                if ip_addr.is_link_local() || ip_addr.is_loopback() {
-                    continue;
-                }
-                if ip_addr.is_private() {
-                    colored_event_data = Some(event_data.clone().bright_purple());
-                    break 'outer;
-                }
-                //if ip_addr.is_global() {
-                    colored_event_data = Some(event_data.clone().bright_red());
-                    break 'outer;
-                //}
-            }
-        }
-    }
 
     let output = match cli.delimiter {
         None => format!(
@@ -121,7 +168,7 @@ fn display_record(record: &SerializedEvtxRecord<Value>, cli: &Cli) -> Result<()>
             record.timestamp.format("%FT%T"),
             size,
             event_id,
-            colored_event_data.as_ref().unwrap_or(&event_data)
+            event_data
         ),
         Some(d) => format!(
             "{}{}{}{}{}{}{}{}{}",
@@ -133,7 +180,7 @@ fn display_record(record: &SerializedEvtxRecord<Value>, cli: &Cli) -> Result<()>
             d,
             event_id,
             d,
-            colored_event_data.as_ref().unwrap_or(&event_data)
+            event_data
         ),
     }.normal();
 
